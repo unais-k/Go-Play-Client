7cf50171494e8558e8017b75a3adda1903c56b7f
unais-k <unaissafa008@gmail.com>
2023-05-27 00:34:30 +0530

    dash

--- src/API/AxiosInstance.js
+++ src/API/AxiosInstance.js
@@ -1,20 +1,20 @@
 import Axios from "axios";
 export const AxiosClient = Axios.create({
-    baseURL: "http://localhost:4001/api/client/",
+    baseURL: "https://go-play-server.onrender.com/api/client/",
 });
 
 export const AxiosAdmin = Axios.create({
-    baseURL: "http://localhost:4001/api/admin",
+    baseURL: "https://go-play-server.onrender.com/api/admin",
 });
 
 export const AxiosTurfAdmin = Axios.create({
-    baseURL: "http://localhost:4001/api/turf-admin",
+    baseURL: "https://go-play-server.onrender.com/api/turf-admin",
 });
 
 export const AxiosConversation = Axios.create({
-    baseURL: "http://localhost:4001/api/conversation",
+    baseURL: "https://go-play-server.onrender.com/api/conversation",
 });
 
 export const AxiosChat = Axios.create({
-    baseURL: "http://localhost:4001/api/chat",
+    baseURL: "https://go-play-server.onrender.com/api/chat",
 });
